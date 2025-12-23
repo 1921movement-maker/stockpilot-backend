58ff0cf8507f93e201206402c208b3e7b4eb0629
1921movement-maker <1921movement@gmail.com>
2025-12-23 01:00:21 -0600

    Update db.js

--- src/routes/db.js
+++ src/routes/db.js
@@ -5,9 +5,9 @@ if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
 }
 
-export const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: { rejectUnauthorized: false },
 });
+
+export default pool;

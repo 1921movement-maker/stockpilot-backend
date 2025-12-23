bfa7aba154731e2bdf1eeb7ae0604987e789c2d7
1921movement-maker <1921movement@gmail.com>
2025-12-22 22:30:41 -0600

    Create shopify.js

---
+++ src/shopify.js
@@ -0,0 +1,10 @@
+import { shopifyApi, LATEST_API_VERSION } from "@shopify/shopify-api";
+
+export const shopify = shopifyApi({
+  apiKey: process.env.SHOPIFY_API_KEY,
+  apiSecretKey: process.env.SHOPIFY_API_SECRET,
+  scopes: (process.env.SCOPES || "").split(",").map(s => s.trim()),
+  hostName: process.env.SHOPIFY_APP_URL.replace(/^https?:\/\//, ""),
+  apiVersion: process.env.SHOPIFY_API_VERSION || LATEST_API_VERSION,
+  isEmbeddedApp: true
+});

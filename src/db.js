97857ca3f343c5bcdfa956db40a0e97a72f00434
1921movement-maker <1921movement@gmail.com>
2025-12-22 22:43:36 -0600

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,5 +1,13 @@
 import pg from "pg";
-export const pool = new pg.Pool({
+const { Pool } = pg;
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
